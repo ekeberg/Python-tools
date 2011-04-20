bd1f50207fe9a2d3b3d610bc26e29a361dd39ff2
Tomas Ekeberg <ekeberg@kandinsky.bmc.uu.se>
2011-04-20 14:25:41 +0200

    Made make_all_python_executable better

--- Scripts/make_all_python_executable.py
+++ Scripts/make_all_python_executable.py
@@ -4,39 +4,44 @@ import sys, os, re, pwd
 
 home = os.path.expanduser('~')
 #home = "/home/%s" % pwd.getpwuid(os.getuid())[0]
+path = "%s/Python/Scripts" % home
 
-l = os.popen("find %s/Scripts" % home).readlines()
-files = [f[:-1] for f in l]
+#l = os.popen("find %s" % path).readlines()
+#files = [f[:-1] for f in l]
 
-expr = re.compile('.py$')
-py_files = filter(expr.search,files)
+#expr = re.compile('.py$')
+#py_files = filter(expr.search,files)
+l = os.listdir(path)
+if "scripts.py" in l: l.remove("scripts.py")
+files = ["%s/%s" % (path,f) for f in l if re.search("\.py$",f)]
+print files
 
 #expr = re.compile('^(.(?!home/ekeberg/Scripts/global))*$')
-expr = re.compile('^(.(?!%s/Scripts/global))*$' % home)
+#expr = re.compile('^(.(?!%s/global))*$' % path)
 #expr = re.compile('(?!^/home/ekeberg/Scripts/global)')
-py_files2 = filter(expr.search,py_files)
+#py_files2 = filter(expr.search,py_files)
 
-py_files2.remove('%s/Scripts/scripts.py' % home)
+#py_files2.remove('%s/scripts.py' % path)
 
 expr = re.compile('/\w+\.py')
 
 names = []
 
-for i in py_files2:
+for i in files:
     m = expr.search(i)
     names.append(i[m.start()+1:m.end()-3])
 
-if not os.path.isdir("%s/Scripts/global" % home):
-    os.mkdir("%s/Scripts/global" % home)
+if not os.path.isdir("%s/global" % path):
+    os.mkdir("%s/global" % path)
 
 
-for i in range(len(py_files2)):
-    os.system("cp %s %s/Scripts/global/python_script_%s" % 
-              (py_files2[i],home,names[i]))
+for i in range(len(files)):
+    os.system("cp %s %s/global/python_script_%s" % 
+              (files[i],path,names[i]))
 
-os.system("chmod 744 %s/Scripts/global/*" % home)
+os.system("chmod 744 %s/global/*" % path)
 
-expr = re.compile('%s/Scripts/global' % home)
+expr = re.compile('%s/global' % path)
 if not expr.search(os.environ['PATH']):
-    print "It's suggested to add %s/Scripts/global to your PATH." % home
+    print "It's suggested to add %s/global to your PATH." % path
 
