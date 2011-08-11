a99fa6cdde29cf2f37344f241fda30ae8b11cbd8
Tomas Ekeberg <ekeberg@kandinsky.bmc.uu.se>
2011-08-11 15:41:57 +0200

    fixed a bug

--- Scratch/Net/tms.py
+++ Scratch/Net/tms.py
@@ -3,6 +3,6 @@ from net_tools import *
 #l = Reciever(5011)
 #l.start()
 
-os.chdir('~/Work/programs/emc/git/emc')
+os.chdir(os.path.expanduser('~/Work/programs/emc/git/emc'))
 runs = RunCommandServer(5011,'./emc','')
 runs.start()
