0071968b3cf6ee9881c1a3c0c3b93edb8d28315e
Tomas Ekeberg <ekeberg@kandinsky.bmc.uu.se>
2012-03-29 08:28:56 +0200

    more updates to required orientations

--- Scratch/required_orientations/required_orientations.py
+++ Scratch/required_orientations/required_orientations.py
@@ -12,6 +12,23 @@ class Setup(object):
         self.ks = 2.*pi*(self._object_diameter/self._resolution)/2.
         self.Ks_int = int(self.Ks)
         self.ks_int = int(self.ks)
+        # the adjusted values are lower since they take inte account that the outer shell has a thickness of 1.
+        self.Ks_adjusted = 4.*pi*(self._object_diameter/self._resolution-0.5)**2/2.
+        self.ks_adjusted = 2.*pi*(self._object_diameter/self._resolution-0.5)/2.
+        self.Ks_adjusted_int = int(self.Ks_adjusted)
+        self.ks_adjusted_int = int(self.ks_adjusted)
+
+    def set_Ks(self, Ks):
+        self.Ks = Ks
+        self.Ks_int = int32(Ks)
+        self.Ks_adjusted = 2.*pi*(sqrt(Ks/2./pi) - 0.5)**2
+        self.Ks_adjusted_int = int(self.Ks_adjusted)
+
+    def set_ks(self, ks):
+        self.ks = ks
+        self.ks_int = int32(ks)
+        self.ks_adjusted = ks - pi/2.
+        self.ks_adjusted_int = int(self.ks_adjusted)
 
     def get_int_ratio(self):
         return int(self._object_diameter/self._resolution*2.)
@@ -21,13 +38,18 @@ class Analytic(object):
         self._setup = setup
 
     def full_coverage(self, number_of_images):
-        return (1. - (1.-self._setup.ks/self._setup.Ks)**number_of_images)**self._setup.Ks
+        return (1. - (1.-self._setup.ks_adjusted/self._setup.Ks_adjusted)**number_of_images)**self._setup.Ks_adjusted
 
     def coverage(self, number_of_images):
-        return 1. - exp(-number_of_images*self._setup.ks/self._setup.Ks)
+        return 1. - exp(-number_of_images*self._setup.ks_adjusted/self._setup.Ks_adjusted)
 
     def N_from_coverage(self, coverage):
-        return -self._setup.Ks/self._setup.ks*log(1.-coverage)
+        return -self._setup.Ks_adjusted/self._setup.ks_adjusted*log(1.-coverage)
+
+    def at_most_n_missing(self, n, number_of_images):
+        p_single = 1. - (1. - float(self._setup.ks_adjusted_int)/float(self._setup.Ks_adjusted_int))**number_of_images
+        return sum([binom(self._setup.Ks_adjusted_int, k)*p_single**k*(1.-p_single)**(self._setup.Ks_adjusted_int-k) for k in range(self._setup.Ks_adjusted_int-n, self._setup.Ks_adjusted_int+1)], axis=0)
+        
 
 class SliceInserter(object):
     def __init__(self, model_side, image_side):
@@ -99,7 +121,7 @@ class SimulationRealistic(object):
         job_results = parallel.run_parallel(jobs, single_repeat)
         return average(job_results, axis=0)
         
-    def full_coverage(self, max_images, number_of_repeats):
+    def full_coverage(self, max_images, number_of_repeats, max_missing=0):
         model_side = self._setup.get_int_ratio()
         image_side = self._setup.get_int_ratio()
         inserter = SliceInserter(model_side, image_side)
@@ -109,7 +131,8 @@ class SimulationRealistic(object):
             full_coverage = zeros(max_images, dtype='bool')
             for image_n in range(max_images):
                 inserter.insert(model, rotations.quaternion_to_matrix(rotations.random_quaternion()))
-                full_coverage[image_n] = (sum(model > 0) >= inserter.max_sum)
+                #full_coverage[image_n] = (sum(model > 0) >= inserter.max_sum)
+                full_coverage[image_n] = (sum(model*inserter.shell_mask > 0) >= inserter.shell_max_sum-max_missing)
             return full_coverage
 
         jobs = [(inserter,)]*number_of_repeats
@@ -125,7 +148,7 @@ class SimulationModel(object):
         # def single_repeat(input):
         #     setup = input[0]; max_images = input[1]
         def single_repeat(setup, max_images):
-            model = zeros(setup.Ks_int)
+            model = zeros(setup.Ks_adjusted_int)
             coverage = zeros(max_images)
             
             def get_n_unique(n, index_range):
@@ -138,20 +161,20 @@ class SimulationModel(object):
                 return index_array
             
             for image_n in range(max_images):
-                indices = get_n_unique(setup.ks_int, setup.Ks_int)
+                indices = get_n_unique(setup.ks_adjusted_int, setup.Ks_adjusted_int)
                 model[indices] = 1.
-                coverage[image_n] = float(sum(model)) / float(setup.Ks_int)
+                coverage[image_n] = float(sum(model)) / float(setup.Ks_adjusted_int)
             return coverage
 
         jobs = [(self._setup, max_images)]*number_of_repeats
         job_results = parallel.run_parallel(jobs, single_repeat)
         return average(job_results, axis=0)
 
-    def full_coverage(self, max_images, number_of_repeats):
+    def full_coverage(self, max_images, number_of_repeats, max_missing=0):
         # def single_repeat(input):
         #     setup = input[0]; max_images = input[1]
         def single_repeat(setup, max_images):
-            model = zeros(setup.Ks_int)
+            model = zeros(setup.Ks_adjusted_int)
             full_coverage = zeros(max_images)
 
             def get_n_unique(n, index_range):
@@ -164,9 +187,9 @@ class SimulationModel(object):
                 return index_array
             
             for image_n in range(max_images):
-                indices = get_n_unique(setup.ks_int, setup.Ks_int)
+                indices = get_n_unique(setup.ks_adjusted_int, setup.Ks_adjusted_int)
                 model[indices] = 1.
-                full_coverage[image_n] = sum(model) == setup.Ks_int
+                full_coverage[image_n] = (sum(model) >= setup.Ks_adjusted_int-max_missing)
             return full_coverage
 
         jobs = [(self._setup, max_images)]*number_of_repeats
@@ -175,18 +198,45 @@ class SimulationModel(object):
                 
 
 if __name__ == "__main__":
-    setup = Setup(83.*20, 83.) #415
-    a = Analytic(setup)
-    sr = SimulationRealistic(setup)
-    sm = SimulationModel(setup)
+    # setup = Setup(83.*15, 83.) #415
+    # a = Analytic(setup)
+    # sm = SimulationModel(setup)
+    # setup = Setup(83.*16, 83.) #415
+    # sr = SimulationRealistic(setup)
 
-    max_images = 600
+
+    max_images = 1300
+    # an_full_coverage = a.full_coverage(arange(max_images))
+    # sr_full_coverage = sr.full_coverage(max_images, 200)
+    # sm_full_coverage = sm.full_coverage(max_images, 200)
+    #an_full_coverage = a.at_most_n_missing(0, arange(max_images))
+    #sr_full_coverage = sr.full_coverage(max_images, 1000)
+    #sm_full_coverage = sm.full_coverage(max_images, 1000)
+
+
+    setup = Setup(83.*32, 83.)
+    a = Analytic(setup)
     an_full_coverage = a.full_coverage(arange(max_images))
-    sr_full_coverage = sr.full_coverage(max_images, 100)
-    sm_full_coverage = sm.full_coverage(max_images, 100)
+    
+    sm = SimulationModel(setup)
+    sm_full_coverage = sm.full_coverage(max_images, 1000)
+    
+    print "Ks = %d, ks = %d" % (setup.Ks, setup.ks)
+    
+    sr = SimulationRealistic(setup)
+    sr_full_coverage = sr.full_coverage(max_images, 1000)
+
+    print "old Ks = %d" % setup.Ks
+    setup.set_Ks(setup.Ks*1.1) #this seems to work pretty well (for some reason)
+    print "new Ks = %d" % setup.Ks
+    
+    #setup.set_ks(48.)
+    sm2 = SimulationModel(setup)
+    sm2_full_coverage = sm.full_coverage(max_images, 1000)
 
     plot(an_full_coverage, label='analytical')
     plot(sr_full_coverage, label='realistic sim')
     plot(sm_full_coverage, label='model sim')
+    plot(sm2_full_coverage, label='model sim adjusted')
     legend()
     show()
