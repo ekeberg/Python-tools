d6e7c2579960c35b702d735a7c44c43c59914cf4
Tomas Ekeberg <ekeberg@icm-241-130.icm.uu.se>
2014-12-12 15:34:40 +0100

    Updates to progress bar in time_tools.

--- Modules/time_tools.py
+++ Modules/time_tools.py
@@ -1,12 +1,15 @@
+"""Toosl dealing with time, such as timing a function and plotting a progress bar."""
 import time
 import datetime
+import sys
 
-class Progress:
-    def __init__(self, message, number_of_iterations, output_period = 1.0):
+class Progress(object):
+    """Simple progress bar implementation"""
+    def __init__(self, message, number_of_iterations, output_period=1.0):
         self._message = message
         self._number_of_iterations = number_of_iterations
         self._tasks_completed = 0
-        self._time = time.clock()*60.
+        self._time = time.time()
         self._last_iteration_time = 0.0
         self._expected_time_left = 0.0
         self._last_output_time = 0.0
@@ -14,46 +17,65 @@ class Progress:
         self._max_output_period = output_period
 
     def start(self):
+        """Call before starting task."""
         self._tasks_completed = 0
-        self._time = time.clock()*60.
+        self._time = time.time()
         self._last_output_time = self._time
 
+    def finished(self):
+        """Call when task is done."""
+        sys.stdout.write("\n")
+
     def iteration_completed(self):
+        """Call after every iteration. This function plots the otuput if a significant
+        time passed since last plot."""
         self._tasks_completed += 1
-        new_time = time.clock()*60.
+        new_time = time.time()
         self._last_iteration_time = new_time - self._time
         self._time = new_time
         self._expected_time_left = self._last_iteration_time * (self._number_of_iterations -
                                                                 self._tasks_completed)
-        if (new_time > (self._last_output_time + self._max_output_period)):
-            print "%s: Iteration tock %g seconds. Expected %g seconds left" % (self._message,
-                                                                               self._last_iteration_time,
-                                                                               self._expected_time_left)
+        if new_time > (self._last_output_time + self._max_output_period):
+            bar_length = 50
+            done_length = int(float(self._tasks_completed) / float(self._number_of_iterations) * bar_length)
+            not_done_length = bar_length - done_length
+            sys.stdout.write("\r[{done}{not_done}] {time_left} seconds left".format(done="#"*done_length, not_done="-"*not_done_length,
+                                                                                    time_left=int(self._expected_time_left)))
+            sys.stdout.flush()
             self._last_output_time = new_time
             self._last_output_index = self._tasks_completed
-        
 
-class StopWatch:
+class StopWatch(object):
+    """Uses wall time."""
     def __init__(self):
         self._running = False
+        self._start_time = None
+        self._end_time = None
+        self._time_diff = None
 
     def start(self):
+        """Start watch"""
         self._running = True
         self._start_time = time.time()
 
     def stop(self):
+        """Stop and reset watch"""
         if not self._running:
-            raise StateError("Timer needs to be running to be stopped")
+            raise RuntimeError("Timer needs to be running to be stopped")
+        self._running = False
         self._end_time = time.time()
         self._time_diff = self._end_time - self._start_time
 
     def time(self):
+        """The time in seconds that the clock was running."""
+        if self._running:
+            self._time_diff = time.time() - self._start_time
         return self._time_diff
 
-
-        if self._time_diff < 1.:
-            return "%g ms" % (self._time_diff*1000.)
+    def time_string(self):
+        """Nicely formated string of the time the clock was running."""
+        time_diff = self.time()
+        if time_diff < 1.:
+            return "{0} ms".format(time_diff*1000.)
         else:
-            return str(datetime.timedelta(seconds=self._time_diff))
-
-        
+            return str(datetime.timedelta(seconds=time_diff))
