5e526e251956af0c96270ddf83831e695dcfcc4f
Tomas Ekeberg <ekeberg@icm-241-130.icm.uu.se>
2014-12-12 15:34:08 +0100

    Fixed bug with default fields in sphelper.

--- Modules/sphelper.py
+++ Modules/sphelper.py
@@ -1,28 +1,40 @@
+"""Functions to interact with spimage h5py files without loading the spimage package."""
 import h5py as _h5py
 import pylab as _pylab
 
-def import_spimage(filename, fields=['image']):
+def import_spimage(filename, fields=('image',)):
     """Import image part of an spimage file"""
 
     def _read_image(name):
-        if file_handle['phased'][...] == 1:
-            image = _pylab.squeeze(file_handle['real'][...] + 1.j*file_handle['imag'][...])
+        """Read an image from a file_handle"""
+        if name == "image":
+            if file_handle["phased"][0]:
+                image = _pylab.squeeze(file_handle['real'][...] + 1.j*file_handle['imag'][...])
+            else:
+                image = _pylab.real(_pylab.squeeze(file_handle['real'][...]))
+        elif name == "mask":
+            image = file_handle["mask"][...]
         else:
-            image = _pylab.real(_pylab.squeeze(file_handle['real'][...]))
+            raise ValueError("Can not load {0}.".format(name))
         return image
+
     def _read_array(name):
+        """Read an array from a file_handle"""
         array = _pylab.squeeze(file_handle[name][...])
         return array
+
     def _read_single(name):
+        """Read an singel value from a file_handle"""
         single = file_handle[name][...][0]
         return single
+
     allowed_fields = {'image' : _read_image, 'mask' : _read_array, 'detector_distance' : _read_single,
                       'image_center' :_read_array, 'lambda' : _read_single,
                       'num_dimensions' : _read_single, 'phased' : _read_single,
                       'pixel_size' : _read_single, 'scaled' : _read_single,
                       'shifted' : _read_single, 'version' : _read_single}
-    field_values  = []
-    with _h5py.File(filename,'r') as file_handle:
+    field_values = []
+    with _h5py.File(filename, 'r') as file_handle:
         for this_field in fields:
             try:
                 field_values.append(allowed_fields[this_field](this_field))
@@ -34,6 +46,7 @@ def import_spimage(filename, fields=['image']):
         return field_values
 
 def save_spimage(image, filename, mask=None):
+    """Create an spimage hdf5 file from an image and optionally a mask."""
     with _h5py.File(filename, 'w') as file_handle:
 
         file_handle['real'] = _pylab.real(image)
@@ -61,6 +74,7 @@ def save_spimage(image, filename, mask=None):
         #file_handle.close()
 
 def read_tiff(filename):
+    """Read a tiff image."""
     import gdal
     # with gdal.Open(filename) as image_handle:
     #     image = image_handle.ReadAsArray()
