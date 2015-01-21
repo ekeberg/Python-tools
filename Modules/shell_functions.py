c7e8ed7097cf9bac8ed4d3a4ddc6de25012e01a4
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 15:20:34 +0100

    Module shell_functions.py: Docstrings added.

--- Modules/shell_functions.py
+++ Modules/shell_functions.py
@@ -1,6 +1,8 @@
+"""Implements a few shell features that are not available natively in python"""
 import os, errno
 
 def mkdir_p(path):
+    """Create a directory without returning an error if it already exists."""
     try:
         os.makedirs(path)
     except OSError as exc: # Python >2.5
