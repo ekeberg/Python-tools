b3336322cdf949cd306784c794b13fa672087790
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 15:12:58 +0100

    Module image_manipulation.py follows standards better.

--- Modules/image_manipulation.py
+++ Modules/image_manipulation.py
@@ -1,148 +1,119 @@
-#from pylab import *
-#from spimage import *
+"""A collection of image manipulation functions. Some of them uses spimage objects
+and some numpy arrays."""
 import pylab
 import spimage
-import sys
 
 def center_image_2d(img, radius):
     """For an image with an object surounded by empty space, this function
     puts it in the center. A rough idea about the size of the object is
     needed in the form of the variable radius."""
-    #sigma = 3
     sigma = radius
-    #img = sp_image_read(filename,0)
-    side = pylab.shape(img.image)[0]
-    x = pylab.arange(pylab.shape(img.image)[0],dtype='float64') -\
-        pylab.shape(img.image)[0]/2.0 + 0.5
-    y = pylab.arange(pylab.shape(img.image)[1],dtype='float64') -\
-        pylab.shape(img.image)[1]/2.0 + 0.5
-    #X,Y,Z = meshgrid(x,y,z)
-    #kernel = X**2+Y**2
-    #kernel = exp(-(X**2+Y**2+Z**2)/2.0/sigma**2)
-    kernel = pylab.exp(-(x[:,pylab.newaxis]**2+y[pylab.newaxis,:]**2)/2.0/sigma**2)
-
-    #img.image[:,:] = kernel[:,:]
-    #sp_image_write(img,"foo_kernel.h5",0)
-    #exit(0)
+    x_coordinates = pylab.arange(pylab.shape(img.image)[0], dtype='float64') -\
+                    pylab.shape(img.image)[0]/2.0 + 0.5
+    y_coordinates = pylab.arange(pylab.shape(img.image)[1], dtype='float64') -\
+                    pylab.shape(img.image)[1]/2.0 + 0.5
+    kernel = pylab.exp(-(x_coordinates[:, pylab.newaxis]**2 +
+                         y_coordinates[pylab.newaxis, :]**2)/2.0/sigma**2)
 
     img_ft = pylab.fft2(pylab.fftshift(img.image))
     kernel_ft = pylab.fft2(pylab.fftshift(kernel))
     kernel_ft *= pylab.conj(img_ft)
-    bt = pylab.ifft2(kernel_ft)
+    img_bt = pylab.ifft2(kernel_ft)
 
     min_v = 0.
     min_x = 0
     min_y = 0
-    for x in range(pylab.shape(bt)[0]):
-        for y in range(pylab.shape(bt)[1]):
-            if abs(bt[y,x]) > min_v:
-                    min_v = abs(bt[y,x])
-                    min_x = x
-                    min_y = y
+    for x_index in range(pylab.shape(img_bt)[0]):
+        for y_index in range(pylab.shape(img_bt)[1]):
+            if abs(img_bt[y_index, x_index]) > min_v:
+                min_v = abs(img_bt[y_index, x_index])
+                min_x = x_index
+                min_y = y_index
     print min_x, min_y
-    spimage.sp_image_translate(img,-(-min_x + pylab.shape(bt)[0]/2),
-                               -(-min_y + pylab.shape(bt)[1]/2),
+    spimage.sp_image_translate(img, -(-min_x + pylab.shape(img_bt)[0]/2),
+                               -(-min_y + pylab.shape(img_bt)[1]/2),
                                0, spimage.SP_TRANSLATE_WRAP_AROUND)
     shift = spimage.sp_image_shift(img)
     spimage.sp_image_free(img)
-    #shift = img
-    #sp_image_write(shift,outfile,0)
     return shift
-
 
 def center_image_3d(img, radius):
     """For an image with an object surounded by empty space, this function
     puts it in the center. A rough idea about the size of the object is
     needed in the form of the variable radius."""
-    #sigma = 3
     sigma = radius
-    #img = sp_image_read(filename,0)
-    side = pylab.shape(img.image)[0]
-    x = pylab.arange(pylab.shape(img.image)[0],dtype='float64') -\
-        pylab.shape(img.image)[0]/2.0 + 0.5
-    y = pylab.arange(pylab.shape(img.image)[1],dtype='float64') -\
-        pylab.shape(img.image)[1]/2.0 + 0.5
-    z = pylab.arange(pylab.shape(img.image)[2],dtype='float64') -\
-        pylab.shape(img.image)[2]/2.0 + 0.5
-    #X,Y,Z = meshgrid(x,y,z)
-    #kernel = X**2+Y**2
-    #kernel = exp(-(X**2+Y**2+Z**2)/2.0/sigma**2)
-    kernel = pylab.exp(-(x[:,pylab.newaxis,pylab.newaxis]**2+
-                         y[pylab.newaxis,:,pylab.newaxis]**2+
-                         z[pylab.newaxis,pylab.newaxis,:]**2)/2.0/sigma**2)
-
-    #img.image[:,:] = kernel[:,:]
-    #sp_image_write(img,"foo_kernel.h5",0)
-    #exit(0)
+    x_coordinates = pylab.arange(pylab.shape(img.image)[0], dtype='float64') -\
+                    pylab.shape(img.image)[0]/2.0 + 0.5
+    y_coordinates = pylab.arange(pylab.shape(img.image)[1], dtype='float64') -\
+                    pylab.shape(img.image)[1]/2.0 + 0.5
+    z_coordinates = pylab.arange(pylab.shape(img.image)[2], dtype='float64') -\
+                    pylab.shape(img.image)[2]/2.0 + 0.5
+    kernel = pylab.exp(-(x_coordinates[:, pylab.newaxis, pylab.newaxis]**2+
+                         y_coordinates[pylab.newaxis, :, pylab.newaxis]**2+
+                         z_coordinates[pylab.newaxis, pylab.newaxis, :]**2)/2.0/sigma**2)
 
     img_ft = pylab.fft2(pylab.fftshift(img.image))
     kernel_ft = pylab.fft2(pylab.fftshift(kernel))
     kernel_ft *= pylab.conj(img_ft)
-    bt = pylab.ifft2(kernel_ft)
+    img_bt = pylab.ifft2(kernel_ft)
 
     min_v = 0.
     min_x = 0
     min_y = 0
     min_z = 0
-    for x in range(pylab.shape(bt)[0]):
-        for y in range(pylab.shape(bt)[1]):
-            for z in range(pylab.shape(bt)[2]):
-                if abs(bt[z,y,x]) > min_v:
-                    min_v = abs(bt[z,y,x])
-                    min_x = x
-                    min_y = y
-                    min_z = z
+    for x_index in range(pylab.shape(img_bt)[0]):
+        for y_index in range(pylab.shape(img_bt)[1]):
+            for z_index in range(pylab.shape(img_bt)[2]):
+                if abs(img_bt[z_index, y_index, x_index]) > min_v:
+                    min_v = abs(img_bt[z_index, y_index, x_index])
+                    min_x = x_index
+                    min_y = y_index
+                    min_z = z_index
     print min_x, min_y, min_z
-    # spimage.sp_image_translate(img,-(-min_z + pylab.shape(bt)[0]/2),
-    #                            -(-min_y + pylab.shape(bt)[1]/2),
-    #                            -(-min_x + pylab.shape(bt)[2]/2), SP_TRANSLATE_WRAP_AROUND)
-    spimage.sp_image_translate(img,-(-min_z + pylab.shape(bt)[0]/2),
-                               -(-min_y + pylab.shape(bt)[1]/2),
-                               -(-min_x + pylab.shape(bt)[2]/2), 1)
-    #shift = sp_image_shift(img)
+    spimage.sp_image_translate(img, -(-min_z + pylab.shape(img_bt)[0]/2),
+                               -(-min_y + pylab.shape(img_bt)[1]/2),
+                               -(-min_x + pylab.shape(img_bt)[2]/2), 1)
     shift = img
 
-    #sp_image_write(shift,outfile,0)
     return shift
 
-def scale_image_2d(img, factor):
+def scale_image_2d(image, factor):
     """Scales up the image by the scaling factor. No cropping is done.
     Input:
     image
     factor"""
-    sizeX = pylab.shape(img.image)[0]
-    sizeY = pylab.shape(img.image)[1]
-    centerX = sizeX/2
-    centerY = sizeY/2
-    windowX = int(sizeX/factor)
-    windowY = int(sizeY/factor)
-    ft = pylab.fft2(img.image[centerX-windowX/2:centerX+windowX/2,
-                              centerY-windowY/2:centerY+windowY/2])
-    rs = abs(pylab.ifftn(pylab.fftshift(ft),[sizeX,sizeY]))
-    return rs
-    #img.image[:,:] = rs[:,:]
+    size_x = pylab.shape(image)[0]
+    size_y = pylab.shape(image)[1]
+    center_x = size_x/2
+    center_y = size_y/2
+    window_x = int(size_x/factor)
+    window_y = int(size_y/factor)
+    image_ft = pylab.fft2(image[center_x-window_x/2:center_x+window_x/2,
+                                center_y-window_y/2:center_y+window_y/2])
+    image_scaled = abs(pylab.ifftn(pylab.fftshift(image_ft), [size_x, size_y]))
 
-def scale_image_3d(img, factor):
+    return image_scaled
+
+def scale_image_3d(image, factor):
     """Scales up the image by the scaling factor.
     Input:
     image
     factor"""
-    sizeX = pylab.shape(img.image)[0]
-    sizeY = pylab.shape(img.image)[1]
-    sizeZ = pylab.shape(img.image)[2]
-    centerX = sizeX/2
-    centerY = sizeY/2
-    centerZ = sizeZ/2
-    windowX = int(sizeX/factor)
-    windowY = int(sizeY/factor)
-    windowZ = int(sizeZ/factor)
-    ft = pylab.fftn(img.image[centerX-windowX/2:centerX+windowX/2,
-                              centerY-windowY/2:centerY+windowY/2,
-                              centerZ-windowZ/2:centerZ+windowZ/2],
-                    [sizeX,sizeY,sizeZ])
-    rs = abs(pylab.ifftn(pylab.fftshift(ft),[sizeX,sizeY,sizeZ]))
-    #return rs
-    img.image[:,:] = rs[:,:]
+    size_x = pylab.shape(image)[0]
+    size_y = pylab.shape(image)[1]
+    size_z = pylab.shape(image)[2]
+    center_x = size_x/2
+    center_y = size_y/2
+    center_z = size_z/2
+    window_x = int(size_x/factor)
+    window_y = int(size_y/factor)
+    window_z = int(size_z/factor)
+    image_ft = pylab.fftn(image[center_x-window_x/2:center_x+window_x/2,
+                                center_y-window_y/2:center_y+window_y/2,
+                                center_z-window_z/2:center_z+window_z/2],
+                          [size_x, size_y, size_z])
+    image_scaled = abs(pylab.ifftn(pylab.fftshift(image_ft), [size_x, size_y, size_z]))
+    return image_scaled
 
 def crop_and_pad(image, center, side):
     """Crops the image around the center to the side given. If the
@@ -153,8 +124,8 @@ def crop_and_pad(image, center, side):
     if len(center) != dims:
         raise ValueError("crop_and_pad: Center must be same length as image dimensions")
 
-    ret = pylab.zeros((side,)*dims,dtype=image.dtype)
-    
+    ret = pylab.zeros((side, )*dims, dtype=image.dtype)
+
     low_in = pylab.array(center)-side/2.0-0.5
     high_in = pylab.array(center)+side/2.0-0.5
 
@@ -170,9 +141,9 @@ def crop_and_pad(image, center, side):
             high_in[i] = pylab.shape(image)[i]
 
     if dims == 2:
-        ret[low_out[0]:high_out[0],low_out[1]:high_out[1]] = image[low_in[0]:high_in[0],low_in[1]:high_in[1]]
+        ret[low_out[0]:high_out[0], low_out[1]:high_out[1]] = image[low_in[0]:high_in[0], low_in[1]:high_in[1]]
     else:
-        ret[low_out[0]:high_out[0],low_out[1]:high_out[1],low_out[2]:high_out[2]] = \
-            image[low_in[0]:high_in[0],low_in[1]:high_in[1],low_in[2]:high_in[2]]
+        ret[low_out[0]:high_out[0], low_out[1]:high_out[1], low_out[2]:high_out[2]] = \
+            image[low_in[0]:high_in[0], low_in[1]:high_in[1], low_in[2]:high_in[2]]
 
     return ret
