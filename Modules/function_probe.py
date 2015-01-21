957b46b6e02adbc9ddc348aadd282837bf789639
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 15:10:51 +0100

    Module function_probe.py follows standards a little bit better.

--- Modules/function_probe.py
+++ Modules/function_probe.py
@@ -1,16 +1,18 @@
 """
-To probe a functions variables, create a container function and pass it as a variable
+To probe a functions variables, create a container object and pass it as a variable
 to the function. Also add the following line at the point you which to probe.
-"exec container._get_code("container")"
+"exec <Container>._get_code("<Container>")"
 """
 
 
-class container(object):
+class Container(object):
+    """Stores function variables to make them accessable outside the function"""
     def __init__(self):
-        code = """
-        l = locals().copy()
-        for k in l:"""
-    def _get_code(self, name):
+        pass
+
+    @classmethod
+    def _get_code(cls, name):
+        """Copies all local variables to the class"""
         return """
 if %s:
     l = locals().copy()
@@ -18,7 +20,9 @@ if %s:
         if k != "%s":
             exec '%s.%s
         """ % (name, name, name, "%s = %s' % (k, k)")
+
     def _reset(self):
+        """Delete stored variables"""
         variables = self.__dict__.copy()
         for variable in variables:
             exec "del self.%s" % variable
