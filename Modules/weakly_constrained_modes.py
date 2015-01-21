2a7a07287745bc5397de3b3991232491c1214825
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 16:22:17 +0100

    Added module weakly_constrained_modes. Contains one function to calculate modes from Fourier and real mask.

---
+++ Modules/weakly_constrained_modes.py
@@ -0,0 +1,48 @@
+"""Calculate weakly constrained modes for a Fourier phase
+retrieval problem using singular value decomposition"""
+import dft
+import numpy
+
+
+def calculate_modes(real_mask, fourier_mask, reality_constraint=False,
+                    number_of_modes=None):
+    """Calculate weakly constrained modes for a Fourier phase retrieval
+    problem using singular value decomposition"""
+    if real_mask.shape != fourier_mask.shape:
+        raise ValueError("Masks must have the same shape")
+
+    real_mask_shifted = numpy.fft.fftshift(real_mask)
+    fourier_mask_shifted = numpy.fft.fftshift(fourier_mask)
+
+    if reality_constraint:
+        sub_matrix = dft.dft_2d_masked_real(real_mask.shape[0],
+                                            real_mask.shape[1],
+                                            real_mask_shifted,
+                                            fourier_mask_shifted)
+    else:
+        sub_matrix = dft.dft_2d_masked(real_mask.shape[0],
+                                       real_mask.shape[1],
+                                       real_mask_shifted,
+                                       fourier_mask_shifted)
+
+    real_modes_raw, singular_values, fourier_modes_raw1 = numpy.linalg.linalg.svd(sub_matrix, full_matrices=False)
+    fourier_modes_raw.dtype = "complex128"
+    inverse_singular_values = numpy.sqrt(1.-singular_values**2)
+
+    if not number_of_modes:
+        number_of_modes = len(singular_values)
+
+    real_modes_shifted = numpy.zeros((number_of_modes,
+                                      real_mask_shifted.shape[0],
+                                      real_mask_shifted.shape[1]),
+                                     dtype="complex128")
+    real_modes_shifted[:, real_mask_shifted] = real_modes_raw.T[:number_of_modes]
+    real_modes = numpy.fft.fftshift(real_modes_shifted, axes=(1, 2))
+    fourier_modes_shifted = numpy.zeros((number_of_modes,
+                                         fourier_mask_shifted.shape[0],
+                                         fourier_mask_shifted.shape[1]),
+                                        dtype="complex128")
+    fourier_modes_shifted[:, fourier_mask_shifted] = fourier_modes_raw[:number_of_modes]
+    fourier_modes = numpy.fft.fftshift(fourier_modes_shifted, axes=(1, 2))
+
+    return inverse_singular_values[:number_of_modes], real_modes, fourier_modes
