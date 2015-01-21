81e879de2d08f6ea7bfcfaee49e8452f4515c588
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 15:21:52 +0100

    Module spimage_tools.py now follows standards better.

--- Modules/spimage_tools.py
+++ Modules/spimage_tools.py
@@ -1,7 +1,9 @@
+"""Collection of tools to make it easier to work with the spimage python interface."""
 import numpy
 import spimage as _spimage
 
 def image_from_array(image, mask=None):
+    """Create spimage image type from numpy array with optional mask"""
     if len(image.shape) == 2:
         img = _spimage.sp_image_alloc(image.shape[0], image.shape[1], 1)
     elif len(image.shape) == 3:
@@ -18,43 +20,50 @@ def image_from_array(image, mask=None):
     return img
 
 def allocate_image(shape):
+    """Allocate spimage image object from numpy type shape"""
     if len(shape) == 2:
         img = _spimage.sp_image_alloc(shape[0], shape[1], 1)
-    elif len(image.shape) == 3:
+    elif len(shape) == 3:
         img = _spimage.sp_image_alloc(shape[0], shape[1], shape[2])
     else:
         raise ValueError("Array must be 2 or 3 dimensional")
     return img
 
-def get_basic_phaser(amplitudes, support):
+def get_basic_phaser(amplitudes, support, mask=None):
     """This function is not finished"""
-    amplitudes_sp = image_from_array(amplitudes)
-    beta = spimage.sp_smap_alloc(1)
-    spimage.sp_smap_insert(beta, 0, 0.9)
-    phase_alg = spimage.sp_phasing_hio_alloc(beta, spimage.SpComplexObject)
+    if mask == None:
+        amplitudes_sp = image_from_array(amplitudes)
+    else:
+        amplitudes_sp = image_from_array(amplitudes, mask)
+    beta = _spimage.sp_smap_alloc(1)
+    _spimage.sp_smap_insert(beta, 0, 0.9)
+    phase_alg = _spimage.sp_phasing_hio_alloc(beta, _spimage.SpNoConstraints)
     support_sp = image_from_array(support)
-    sup_alg = spimage.sp_support_array_init(spimage.sp_support_static_alloc(), 20)
+    sup_alg = _spimage.sp_support_array_init(_spimage.sp_support_static_alloc(), 20)
 
-    phaser = spimage.sp_phaser_alloc()
-    spimage.sp_phaser_init(phaser, phase_alg, sup_alg, spimage.SpEngineCUDA)
-    spimage.sp_phaser_set_amplitudes(phaser, amplitudes_sp)
-    spimage.sp_phaser_init_model(phaser, None, spimage.SpModelRandomPhases)
-    spimage.sp_phaser_init_support(phaser, support_sp, 0, 0)
-    
+    phaser = _spimage.sp_phaser_alloc()
+    _spimage.sp_phaser_init(phaser, phase_alg, sup_alg, _spimage.SpEngineCUDA)
+    _spimage.sp_phaser_set_amplitudes(phaser, amplitudes_sp)
+    _spimage.sp_phaser_init_model(phaser, None, _spimage.SpModelRandomPhases)
+    _spimage.sp_phaser_init_support(phaser, support_sp, 0, 0)
+
     return phaser
 
-def smap(input):
+def smap(values):
+    """Create spimage smap object from iterable of (iteration, value) pairs."""
     import collections
-    if not isinstance(input, collections.Iterable):
-        input = [(0, input)]
+    if not isinstance(values, collections.Iterable):
+        values = [(0, values)]
 
-    smap = _spimage.sp_smap_alloc(len(input))
-    for i in input:
-        _spimage.sp_smap_insert(smap, i[0], i[1])
+    smap_out = _spimage.sp_smap_alloc(len(values))
+    for i in values:
+        _spimage.sp_smap_insert(smap_out, i[0], i[1])
 
-    return smap
+    return smap_out
 
 def get_constraints(real=False, positive=False):
+    """It is easy to forget the name of the constraints variable. This function
+    returns the appropriate one."""
     if real and positive:
         return _spimage.SpPositiveRealObject
     elif real:
@@ -63,35 +72,41 @@ def get_constraints(real=False, positive=False):
         return _spimage.SpPositiveComplexObject
     else:
         return _spimage.SpNoConstraints
-    
+
 def algorithm_hio(beta, real=False, positive=False):
+    """Create an HIO algorithm object"""
     beta_sp = smap(beta)
     constraints = get_constraints(real, positive)
     algorithm = _spimage.sp_phasing_hio_alloc(beta_sp, constraints)
     return algorithm
-    
+
 def algorithm_raar(beta, real=False, positive=False):
+    """Create a RAAR algorithm object"""
     beta_sp = smap(beta)
     constraints = get_constraints(real, positive)
     algorithm = _spimage.sp_phasing_raar_alloc(beta_sp, constraints)
     return algorithm
 
 def algorithm_er(real=False, positive=False):
+    """Create an ER algorithm object"""
     constraints = get_constraints(real, positive)
     algorithm = _spimage.sp_phasing_er_alloc(constraints)
     return algorithm
 
 def support_static():
+    """Create a support update array with only a static support."""
     algorithm = _spimage.sp_support_array_init(_spimage.sp_support_static_alloc(), 20)
     return algorithm
 
 def support_area(area, blur_radius):
+    """Create a support update array with only an area support."""
     area_sp = smap(area)
     blur_radius_sp = smap(blur_radius)
     algorithm = _spimage.sp_support_array_init(_spimage.sp_support_area_alloc(blur_radius_sp, area_sp), 20)
     return algorithm
 
 def support_threshold(threshold, blur_radius):
+    """Create a support update array with only a threshold support."""
     threshold_sp = smap(threshold)
     blur_radius_sp = smap(blur_radius)
     algorithm = _spimage.sp_support_array_init(_spimage.sp_support_threshold_alloc(blur_radius_sp, threshold_sp), 20)
