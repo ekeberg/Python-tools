5f180af00e5a04aaffedf81db81fbf83c0a1cf99
Tomas Ekeberg <ekeberg@icm-39-36.icm.uu.se>
2015-01-21 15:09:44 +0100

    Module elser_particles.py follows standards better.

--- Modules/elser_particles.py
+++ Modules/elser_particles.py
@@ -1,20 +1,25 @@
+"""Generate binary contrast particles with a tunable feature size."""
 import pylab
 
 def elser_particle(array_size, particle_size, feature_size):
-    """Return a binary contrast particle. 'particle_size' and 'feature_size' should both be given in pixels."""
-    if (particle_size > array_size-2): raise ValueError("Particle size must be <= array_size is (%d > %d-2)" % (particle_size, array_size))
+    """Return a binary contrast particle. 'particle_size' and 'feature_size'
+    should both be given in pixels."""
+    if particle_size > array_size-2:
+        raise ValueError("Particle size must be <= array_size is (%d > %d-2)" % (particle_size, array_size))
 
-    x = pylab.arange(array_size) - array_size/2. + 0.5
-    y = pylab.arange(array_size) - array_size/2. + 0.5
-    z = pylab.arange(array_size) - array_size/2. + 0.5
-    r = pylab.sqrt(x**2 + y[:,pylab.newaxis]**2 + z[:,pylab.newaxis,pylab.newaxis]**2)
-    particle_mask = r > particle_size/2.
-    lp_mask = pylab.fftshift(r > array_size/(feature_size*4.))
-    lp_kernel = pylab.fftshift(pylab.exp(-r**2*float(feature_size)**2/float(array_size)**2))
+    x_coordinates = pylab.arange(array_size) - array_size/2. + 0.5
+    y_coordinates = pylab.arange(array_size) - array_size/2. + 0.5
+    z_coordinates = pylab.arange(array_size) - array_size/2. + 0.5
+    radius = pylab.sqrt(x_coordinates[pylab.newaxis, pylab.newaxis, :]**2 +
+                        y_coordinates[pylab.newaxis, :, pylab.newaxis]**2 +
+                        z_coordinates[:, pylab.newaxis, pylab.newaxis]**2)
+    particle_mask = radius > particle_size/2.
+    #lp_mask = pylab.fftshift(radius > array_size/(feature_size*4.))
+    lp_kernel = pylab.fftshift(pylab.exp(-radius**2*float(feature_size)**2/float(array_size)**2))
 
     particle = pylab.random((array_size, )*3)
 
-    for i in range(4):
+    for _ in xrange(4):
         # binary constrast
         particle_average = pylab.median(particle[-particle_mask])
         particle[particle > particle_average] = 1.
@@ -23,26 +28,29 @@ def elser_particle(array_size, particle_size, feature_size):
         # smoothen
         particle_ft = pylab.fftn(particle)
         particle_ft *= lp_kernel
-        particle[:,:] = abs(pylab.ifftn(particle_ft))
-        
+        particle[:, :] = abs(pylab.ifftn(particle_ft))
+
     return particle
 
-def elser_particle_old(resolution):
-    x = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
-    y = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
-    z = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
-    r = pylab.sqrt(x**2+y[:,pylab.newaxis]**2+z[:,pylab.newaxis,pylab.newaxis]**2)
-    particle_mask = r > resolution/2.0
-    lp_mask = pylab.fftshift(r > resolution/4.0)
-    
-    particle = pylab.random((resolution+2,resolution+2,resolution+2))
+def _elser_particle_old(resolution):
+    """This is the original function from Veit Elser without the
+    tunable feature size."""
+    x_coordinates = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
+    y_coordinates = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
+    z_coordinates = pylab.arange(resolution+2) - (resolution+2)/2.0 + 0.5
+    radius = pylab.sqrt(x_coordinates[pylab.newaxis, pylab.newaxis, :]**2 +
+                        y_coordinates[pylab.newaxis, :, pylab.newaxis]**2 +
+                        z_coordinates[:, pylab.newaxis, pylab.newaxis]**2)
+    particle_mask = radius > resolution/2.0
+    lp_mask = pylab.fftshift(radius > resolution/4.0)
 
+    particle = pylab.random((resolution+2, resolution+2, resolution+2))
 
-    for i in range(4):
+    for _ in xrange(4):
         particle[particle_mask] = 0.0
         particle_ft = pylab.fftn(particle)
         particle_ft[lp_mask] = 0.0
-        particle[:,:] = abs(pylab.ifftn(particle))
+        particle[:, :] = abs(pylab.ifftn(particle))
         particle_average = pylab.average(particle.flatten())
         particle[particle > 0.5*particle_average] = 1.0
         particle[particle <= 0.5*particle_average] = 0.0
@@ -51,16 +59,16 @@ def elser_particle_old(resolution):
     return particle
 
 
-if __name__ == "__main__":
-    images = []
-    images_big = []
-    images_binary = []
-    sides = (2**pylab.arange(7))[1:]
-    for s in sides:
-        print "generate side %d particle" % s
-        images.append(elser_particle(s))
-        image_ft = pylab.fftn(images[-1])
-        images_big.append(abs(pylab.ifftn(pylab.fftshift(image_ft),[sides[-1],sides[-1],sides[-1]])))
-        images_binary.append(images_big[-1] > 1.5*pylab.average(images_big[-1].flatten()))
+# if __name__ == "__main__":
+#     images = []
+#     images_big = []
+#     images_binary = []
+#     sides = (2**pylab.arange(7))[1:]
+#     for s in sides:
+#         print "generate side %d particle" % s
+#         images.append(elser_particle(s))
+#         image_ft = pylab.fftn(images[-1])
+#         images_big.append(abs(pylab.ifftn(pylab.fftshift(image_ft), [sides[-1], sides[-1], sides[-1]])))
+#         images_binary.append(images_big[-1] > 1.5*pylab.average(images_big[-1].flatten()))
 
 
