14892c3bce871e498b6e76b6655dcefff875c3f1
Tomas Ekeberg <ekeberg@icm-241-133.icm.uu.se>
2015-01-22 13:26:25 +0100

    Backwards incompatible change: Created one package called eke containing all modules. Added setup.py script for installation. Resources is still handled by raw link in this version so the restructuring is still a work in progress.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+
+# modules = ["QtVersions",
+#            "constants",
+#            "conversions",
+#            "do_prtfs",
+#            "elements",
+#            "elser_particles",
+#            "function_probe",
+#            "gui",
+#            "icosahedral_sphere",
+#            "image_manipulation",
+#            "parallel",
+#            "parse_libconfig",
+#            "rotations"
+#            "shell_functions"
+#            "sphelper"
+#            "spimage_tools"
+#            "time_tools"
+#            "tools"
+#            "vtk_tools"]
+
+setup(name = "python-tools",
+      version = "1.0",
+      packages=["eke"])
