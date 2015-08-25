dd9b20112fe6b4e4df27b8451f6ff3ff24e086e5
Tomas Ekeberg <ekeberg@icm-39-32.icm.uu.se>
2015-08-25 15:42:17 +0200

    Fixed bug in matplotlib_tools.py

--- eke/matplotlib_tools.py
+++ eke/matplotlib_tools.py
@@ -4,10 +4,12 @@ import matplotlib
 
 def scatterplot_dense(axes, x, y, s=100, cmap="YlGnBu_r"):
     xy = numpy.vstack([x, y])
-    z = scipy.stats.gaussian_kde(xy, 0.03)(xy)
+    #z = scipy.stats.gaussian_kde(xy, 0.03)(xy)
+    sigma = numpy.mean((x.max() - x.min(), y.max() - y.min())) * s / 10000
+    z = scipy.stats.gaussian_kde(xy, sigma)(xy)
     idx = z.argsort()
     x_sorted = x[idx]
     y_sorted = y[idx]
     z_sorted = z[idx]
-    scatter_plot = axes.scatter(x, y, c=z, s=s, edgecolor='', cmap=cmap)
+    scatter_plot = axes.scatter(x_sorted, y_sorted, c=z_sorted, s=s, edgecolor='', cmap=cmap)
     return scatter_plot
