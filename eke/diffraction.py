3a0b060fabb1ecb0b9b06834d68b7e10b82aa614
Tomas Ekeberg <ekeberg@icm-241-153.icm.uu.se>
2015-11-06 12:44:48 +0100

    Updated eke.diffraction

--- eke/diffraction.py
+++ eke/diffraction.py
@@ -1,6 +1,9 @@
 import numpy as _numpy
-import rotations as _rotations
 import nfft as _nfft
+import rotations as _rotations
+import constants as _constants
+import conversions as _conversions
+
 
 def ewald_coordinates(image_shape, wavelength, detector_distance, pixel_size):
     pixels_to_im = pixel_size/detector_distance/wavelength
@@ -9,7 +12,6 @@ def ewald_coordinates(image_shape, wavelength, detector_distance, pixel_size):
     x0 = x0_pixels*pixels_to_im
     x1 = x1_pixels*pixels_to_im
     r_pixels = _numpy.sqrt(x0_pixels[:, _numpy.newaxis]**2 + x1_pixels[_numpy.newaxis, :]**2)
-    #r = r_pixels*pixels_to_im
     theta = _numpy.arctan(r_pixels*pixel_size / detector_distance)
     x2 = 1./wavelength*(1 - _numpy.cos(theta))
 
@@ -20,18 +22,22 @@ def ewald_coordinates(image_shape, wavelength, detector_distance, pixel_size):
     output_coordinates[:, 2] = x2.flatten()
     return output_coordinates
 
-def calculate_diffraction(electron_density, density_pixel_size, rotation, image_shape, wavelength, detector_distance, pixel_size):
+
+def calculate_diffraction(scattering_factor_density, density_pixel_size, rotation,
+                          image_shape, wavelength, detector_distance, pixel_size):
     base_coordinates = ewald_coordinates(image_shape, wavelength, detector_distance, pixel_size)
     rotated_coordinates = _rotations.rotate_array(rotation, base_coordinates)
     largest_distance_in_fourier_space = 1./(2.*density_pixel_size)
     scaled_coordinates = rotated_coordinates * 0.5 / largest_distance_in_fourier_space
-    diffraction = _nfft.nfft(electron_density, scaled_coordinates)
+    diffraction = _nfft.nfft(scattering_factor_density, scaled_coordinates)
     return diffraction.reshape(image_shape)
 
-def calculate_diffraction(scattering_factor_density, density_pixel_size, rotation, image_shape, wavelength, detector_distance, pixel_size):
-    base_coordinates = ewald_coordinates(image_shape, wavelength, detector_distance, pixel_size)
-    rotated_coordinates = _rotations.rotate_array(rotation, base_coordinates)
-    largest_distance_in_fourier_space = 1./(2.*density_pixel_size)
-    scaled_coordinates = rotated_coordinates * 0.5 / largest_distance_in_fourier_space
-    diffraction = _nfft.nfft(electron_density, scaled_coordinates)
-    return diffraction.reshape(image_shape)
+
+def klein_nishina(energy, scattering_angle, polarization_angle):
+    """The cross section of a free electron. Energy given in eV, angles given in radians."""
+    energy_in_joules = _conversions.ev_to_J(energy)
+    relative_energy_change = 1. / (1. + (energy_in_joules / (_constants.me*_constants.c**2)) *
+                                   (1. - _numpy.cos(scattering_angle)))
+    return ((_constants.re**2 * relative_energy_change**2)/2. *
+            (relative_energy_change + 1./relative_energy_change -
+             _numpy.sin(scattering_angle)**2*_numpy.cos(polarization_angle)**2))
