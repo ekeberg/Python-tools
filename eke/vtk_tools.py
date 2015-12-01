47d0258af1b15cfbc01de48fb429e19ad8951447
Tomas Ekeberg <ekeberg@Tomass-MacBook-Pro.local>
2015-12-01 18:36:44 +0900

    Changed import names in vtk_tools.

--- eke/vtk_tools.py
+++ eke/vtk_tools.py
@@ -1,15 +1,15 @@
 """Functions and classes that are useful when working with VTK. Uses vtk 6."""
-import vtk
-import numpy
+import vtk as _vtk
+import numpy as _numpy
 
 def get_lookup_table(minimum_value, maximum_value, log=False, colorscale="jet", number_of_colors=1000):
     """Returrns a vtk lookup_table based on the specified matplotlib colorscale"""
     import matplotlib
     import matplotlib.cm
     if log:
-        lut = vtk.vtkLogLookupTable()
+        lut = _vtk.vtkLogLookupTable()
     else:
-        lut = vtk.vtkLookupTable()
+        lut = _vtk.vtkLookupTable()
     lut.SetTableRange(minimum_value, maximum_value)
     lut.SetNumberOfColors(number_of_colors)
     lut.Build()
@@ -23,23 +23,23 @@ def vtk_from_array(array_in, dtype=None):
     if dtype == None:
         dtype = array_in.dtype
     if dtype == "float32":
-        float_array = vtk.vtkFloatArray()
+        float_array = _vtk.vtkFloatArray()
     elif dtype == "float64":
-        float_array = vtk.vtkDoubleArray()
+        float_array = _vtk.vtkDoubleArray()
     else:
         raise ValueError("Wrong format of input array, must be float32 or float64")
     float_array.SetNumberOfComponents(1)
-    float_array.SetVoidArray(numpy.ascontiguousarray(array_in, dtype))
+    float_array.SetVoidArray(_numpy.ascontiguousarray(array_in, dtype))
     return float_array
 
 def window_to_png(render_window, file_name, magnification=1):
-    window_to_image_filter = vtk.vtkWindowToImageFilter()
+    window_to_image_filter = _vtk.vtkWindowToImageFilter()
     window_to_image_filter.SetInput(render_window)
     window_to_image_filter.SetMagnification(magnification)
     window_to_image_filter.SetInputBufferTypeToRGBA()
     window_to_image_filter.Update()
 
-    writer = vtk.vtkPNGWriter()
+    writer = _vtk.vtkPNGWriter()
     writer.SetFileName(file_name)
     writer.SetInputConnection(window_to_image_filter.GetOutputPort())
     writer.Write()
@@ -115,7 +115,7 @@ class SphereMap(object):
         edges = []
         edge_indices = []
         for coordinate_1, coordinate_2 in itertools.combinations(enumerate(coordinates), 2):
-            if ((coordinate_1[1] == coordinate_2[1]).sum() < 3) and (numpy.linalg.norm(coordinate_1[1] - coordinate_2[1]) < 3.):
+            if ((coordinate_1[1] == coordinate_2[1]).sum() < 3) and (_numpy.linalg.norm(coordinate_1[1] - coordinate_2[1]) < 3.):
                 edges.append((coordinate_1[1], coordinate_2[1]))
                 edge_indices.append((coordinate_1[0], coordinate_2[0]))
 
@@ -132,16 +132,16 @@ class SphereMap(object):
                  ((coordinate_1[1] == coordinate_3[1]).sum() < 3) and
                  ((coordinate_2[1] == coordinate_3[1]).sum() < 3)):
                 center = (coordinate_1[1]+coordinate_2[1]+coordinate_3[1])/3.
-                if  (numpy.linalg.norm(center-coordinate_1[1]) < face_cutoff and
-                     numpy.linalg.norm(center-coordinate_2[1]) < face_cutoff and
-                     numpy.linalg.norm(center-coordinate_3[1]) < face_cutoff):
+                if  (_numpy.linalg.norm(center-coordinate_1[1]) < face_cutoff and
+                     _numpy.linalg.norm(center-coordinate_2[1]) < face_cutoff and
+                     _numpy.linalg.norm(center-coordinate_3[1]) < face_cutoff):
                     faces.append((coordinate_1[1], coordinate_2[1], coordinate_3[1]))
                     face_indices.append((coordinate_1[0], coordinate_2[0], coordinate_3[0]))
 
         for edge_index, edge in zip(edge_indices, edges):
             diff = edge[1] - edge[0]
             edge_table[edge_index].append(edge_index[0])
-            for edge_position in numpy.linspace(0., 1., self._sampling_n+1)[1:-1]:
+            for edge_position in _numpy.linspace(0., 1., self._sampling_n+1)[1:-1]:
                 full_coordinate_list.append(edge[0] + diff*edge_position)
                 edge_table[edge_index].append(len(full_coordinate_list)-1)
             edge_table[edge_index].append(edge_index[1])
@@ -158,8 +158,8 @@ class SphereMap(object):
             base_0 = full_coordinate_list[edge_0[-1]] - origin
             base_1 = full_coordinate_list[edge_1[-1]] - origin
 
-            base_1o = numpy.cross(origin, base_0)
-            dot_product = numpy.dot(base_1o, base_1)
+            base_1o = _numpy.cross(origin, base_0)
+            dot_product = _numpy.dot(base_1o, base_1)
             if dot_product < 0.:
                 tmp = base_0
                 base_0 = base_1
@@ -215,32 +215,32 @@ class SphereMap(object):
                                          current_face_offset+self._get_inner_index(index_0-1, index_1+1)))
 
         for point in full_coordinate_list:
-            point /= numpy.linalg.norm(point)
+            point /= _numpy.linalg.norm(point)
 
-        self._points = numpy.array(full_coordinate_list)
-        self._polygons = numpy.array(polygons)
+        self._points = _numpy.array(full_coordinate_list)
+        self._polygons = _numpy.array(polygons)
 
     def _generate_vtk_objects(self):
         """Generate vtk_points, vtk_polygons, vtk_poly_data, vtk_scalars,
         vtk_mapper and vtk_actor.
         This function must be called after _generate_points_and_polys()"""
-        self._vtk_points = vtk.vtkPoints()
+        self._vtk_points = _vtk.vtkPoints()
         for coordinates in self._points:
             self._vtk_points.InsertNextPoint(coordinates[0], coordinates[1], coordinates[2])
 
-        self._vtk_polygons = vtk.vtkCellArray()
+        self._vtk_polygons = _vtk.vtkCellArray()
         for polygon in self._polygons:
-            vtk_polygon = vtk.vtkPolygon()
+            vtk_polygon = _vtk.vtkPolygon()
             vtk_polygon.GetPointIds().SetNumberOfIds(3)
             for local_index, global_index in enumerate(polygon):
                 vtk_polygon.GetPointIds().SetId(local_index, global_index)
             self._vtk_polygons.InsertNextCell(vtk_polygon)
 
-        self._vtk_poly_data = vtk.vtkPolyData()
+        self._vtk_poly_data = _vtk.vtkPolyData()
         self._vtk_poly_data.SetPoints(self._vtk_points)
         self._vtk_poly_data.SetPolys(self._vtk_polygons)
 
-        self._vtk_scalars = vtk.vtkFloatArray()
+        self._vtk_scalars = _vtk.vtkFloatArray()
         self._vtk_scalars.SetNumberOfValues(self._vtk_poly_data.GetPoints().GetNumberOfPoints())
         for i in range(self._vtk_scalars.GetNumberOfTuples()):
             self._vtk_scalars.SetValue(i, 0.)
@@ -248,14 +248,14 @@ class SphereMap(object):
         self._vtk_poly_data.GetPointData().SetScalars(self._vtk_scalars)
         self._vtk_poly_data.Modified()
 
-        self._vtk_mapper = vtk.vtkPolyDataMapper()
+        self._vtk_mapper = _vtk.vtkPolyDataMapper()
         self._vtk_mapper.SetInputData(self._vtk_poly_data)
         self._vtk_mapper.InterpolateScalarsBeforeMappingOn()
         self._vtk_mapper.UseLookupTableScalarRangeOn()
-        self._vtk_actor = vtk.vtkActor()
+        self._vtk_actor = _vtk.vtkActor()
         self._vtk_actor.SetMapper(self._vtk_mapper)
 
-        normals = vtk.vtkFloatArray()
+        normals = _vtk.vtkFloatArray()
         normals.SetNumberOfComponents(3)
         for point in self._points:
             normals.InsertNextTuple(point)
@@ -268,13 +268,13 @@ class SphereMap(object):
         # self._vtk_points.SetNumberOfPoints(len(self._points))
         # for i, c in enumerate(self._points):
         #     self._vtk_points.InsertPoint(i, c[0], c[1], c[2])
-        self._vtk_points = vtk.vtkPoints()
+        self._vtk_points = _vtk.vtkPoints()
         for coordinates in self._points:
             self._vtk_points.InsertNextPoint(coordinates[0], coordinates[1], coordinates[2])
 
-        self._vtk_polygons = vtk.vtkCellArray()
+        self._vtk_polygons = _vtk.vtkCellArray()
         for polygon in self._polygons:
-            vtk_polygon = vtk.vtkPolygon()
+            vtk_polygon = _vtk.vtkPolygon()
             vtk_polygon.GetPointIds().SetNumberOfIds(3)
             for local_index, global_index in enumerate(polygon):
                 vtk_polygon.GetPointIds().SetId(local_index, global_index)
@@ -283,7 +283,7 @@ class SphereMap(object):
         self._vtk_poly_data.SetPoints(self._vtk_points)
         self._vtk_poly_data.SetPolys(self._vtk_polygons)
 
-        self._vtk_scalars = vtk.vtkFloatArray()
+        self._vtk_scalars = _vtk.vtkFloatArray()
         self._vtk_scalars.SetNumberOfValues(self._vtk_poly_data.GetPoints().GetNumberOfPoints())
         for i in range(self._vtk_scalars.GetNumberOfTuples()):
             self._vtk_scalars.SetValue(i, 0.)
@@ -295,7 +295,7 @@ class SphereMap(object):
 class IsoSurface(object):
     """!!Ths function is not done!! Plot an isosurface."""
     def __init__(self, volume):
-        self._surface_level = numpy.mean((volume.min(), volume.max()))
+        self._surface_level = _numpy.mean((volume.min(), volume.max()))
         self._actor = None
         self._volume_scalars = None
         self._image_shape = None
@@ -337,30 +337,30 @@ class IsoSurface(object):
         """Generate the vtkImageData object and connect it to a numpy array."""
         self._volume_max = 1.
 
-        self._volume = vtk.vtkImageData()
+        self._volume = _vtk.vtkImageData()
         self._volume.SetDimensions(*self._image_shape)
 
-        self._volume_scalars = vtk.vtkFloatArray()
-        self._volume_scalars.SetNumberOfValues(numpy.product(self._image_shape))
+        self._volume_scalars = _vtk.vtkFloatArray()
+        self._volume_scalars.SetNumberOfValues(_numpy.product(self._image_shape))
         self._volume_scalars.SetNumberOfComponents(1)
         self._volume_scalars.SetName("FooName")
 
-        for i in range(numpy.product(self._image_shape)):
+        for i in range(_numpy.product(self._image_shape)):
             self._volume_scalars.SetValue(i, self._volume_array)
 
         self._volume.GetPointData().SetScalars(self._volume_scalars)
 
     def _setup_surface(self):
         """create the isosurface object and plotting objects."""
-        self._algorithm = vtk.vtkMarchingCubes()
+        self._algorithm = _vtk.vtkMarchingCubes()
         self._algorithm.SetInputData(self._volume)
         self._algorithm.ComputeNormalsOn()
         self._algorithm.SetValue(0, self._surface_level)
 
-        self._mapper = vtk.vtkPolyDataMapper()
+        self._mapper = _vtk.vtkPolyDataMapper()
         self._mapper.SetInputConnection(self._algorithm.GetOutputPort())
         self._mapper.ScalarVisibilityOff()
-        self._actor = vtk.vtkActor()
+        self._actor = _vtk.vtkActor()
         self._actor.GetProperty().SetColor(*self._color)
         self._actor.SetMapper(self._mapper)
 
