91971dc2f99f36bd5b5afbeec2b2b77e0db6878a
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2016-01-26 11:24:01 +0100

    Removed unneccesary prints in hdf5_tools.

--- eke/hdf5_tools.py
+++ eke/hdf5_tools.py
@@ -5,11 +5,9 @@ def dictionary_to_hdf5(file_name, dict_in):
     def write_group(group_handle, dict_in):
         for name, value in dict_in.iteritems():
             if isinstance(value, dict):
-                print "create group: {0}".format(name)
                 new_group = group_handle.create_group(name)
                 write_group(new_group, value)
             else:
-                print "create dataset {0}".format(name)
                 group_handle.create_dataset(name, data=_numpy.array(value))
     with _h5py.File(file_name, "w") as file_handle:
         write_group(file_handle, dict_in)
@@ -19,10 +17,8 @@ def hdf5_to_dictionary(file_name):
         return_dictionary = {}
         for name, value in group.iteritems():
             if isinstance(value, _h5py.Dataset):
-                print "read dataset {0}".format(name)
                 return_dictionary[name] = value[...]
             elif isinstance(value, _h5py.Group):
-                print "read group {0}".format(name)
                 return_dictionary[name] = read_group(value)
             else:
                 raise IOError("Can't handle {0} in file {1}".format(str(type(value))), file_name)
@@ -30,3 +26,12 @@ def hdf5_to_dictionary(file_name):
     with _h5py.File(file_name, "r") as file_handle:
         return_dictionary = read_group(file_handle)
     return return_dictionary
+
+def save_numpy(filename, array):
+    with _h5py.File(filename, "w") as file_handle:
+        file_handle.create_dataset("data", data=_numpy.array(array))
+
+def read_numpy(filename):
+    with _h5py.File(filename, "r") as file_handle:
+        array = file_handle["data"][...]
+    return array
