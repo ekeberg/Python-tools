6d672909cf991fc7a7744993de15512f45d389a3
Tomas Ekeberg <ekeberg@eduroam-0113.desy.de>
2016-03-24 10:13:56 +0100

    Added missing shift in kernel kreation.

--- eke/phasing_tools.py
+++ eke/phasing_tools.py
@@ -4,4 +4,4 @@ def get_edge_kernel(image_shape, center=None):
     """The image must be centered before applying this kernel"""
     a = _numpy.array([0.25, 0.5, 0.25])
     kernel = a[:, _numpy.newaxis] * a[_numpy.newaxis, :]
-    return _numpy.fft.fftn(kernel, image_shape)
+    return abs(_numpy.fft.fftshift(_numpy.fft.fftn(_numpy.fft.fftshift(kernel), image_shape)))
