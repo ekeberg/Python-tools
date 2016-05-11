e0aa3282ad8e54c65f12d0e36ce76d6086568a5c
Tomas Ekeberg <ekeberg@icm-39-27.icm.uu.se>
2016-05-11 13:37:35 +0200

    Fixed bug in slurm_tools.py

--- eke/slurm_tools.py
+++ eke/slurm_tools.py
@@ -2,25 +2,25 @@ import os as _os
 
 def get_array_index():
     try:
-        return _os.environ["SBATCH_ARRAY_TASK_ID"]
+        return _os.environ["SLURM_ARRAY_TASK_ID"]
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_size():
     try:
-        return _os.environ["SBATCH_ARRAY_TASK_MAX"] - _os.environ["SBATCH_ARRAY_TASK_MIN"]
+        return _os.environ["SLURM_ARRAY_TASK_MAX"] - _os.environ["SLURM_ARRAY_TASK_MIN"]
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_min():
     try:
-        return _os.environ["SBATCH_ARRAY_TASK_MIN"]
+        return _os.environ["SLURM_ARRAY_TASK_MIN"]
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_max():
     try:
-        return _os.environ["SBATCH_ARRAY_TASK_MAX"]
+        return _os.environ["SLURM_ARRAY_TASK_MAX"]
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
