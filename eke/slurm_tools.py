042005a58d1feb4abc2b7e752528b6a52cc9f500
Tomas Ekeberg <ekeberg@eduroam-0596.desy.de>
2016-09-22 14:47:24 +0200

    Added function in slurm_tools to get many indices from a single one and to invert it.

--- eke/slurm_tools.py
+++ eke/slurm_tools.py
@@ -1,27 +1,52 @@
 import os as _os
+import numpy as _numpy
 
 def get_array_index():
     try:
-        return _os.environ["SLURM_ARRAY_TASK_ID"]
+        return int(_os.environ["SLURM_ARRAY_TASK_ID"])
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_size():
     try:
-        return _os.environ["SLURM_ARRAY_TASK_MAX"] - _os.environ["SLURM_ARRAY_TASK_MIN"]
+        return int(_os.environ["SLURM_ARRAY_TASK_MAX"]) - int(_os.environ["SLURM_ARRAY_TASK_MIN"])
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_min():
     try:
-        return _os.environ["SLURM_ARRAY_TASK_MIN"]
+        return int(_os.environ["SLURM_ARRAY_TASK_MIN"])
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 def get_array_max():
     try:
-        return _os.environ["SLURM_ARRAY_TASK_MAX"]
+        return int(_os.environ["SLURM_ARRAY_TASK_MAX"])
     except KeyError:
         raise EnvironmentError("This process is not part of a slurm array")
 
 
+def index_to_multiple(index, lengths):
+    """Provide a single index (such as array number) and get
+    multiple indices back."""
+    lengths = _numpy.int64(_numpy.array(lengths))
+    if index < 0 or index >= _numpy.prod(lengths):
+        raise ValueError("Index is out of range")
+    result = []
+    for i, length in enumerate(lengths):
+        div = _numpy.prod(lengths[i+1:])
+        mod = length
+        result.append((index / div) % mod)
+    return tuple(result)
+
+def multiple_to_index(multiple, lengths):
+    """Provide a tuple with multiple indices (such as array number) and get
+    a single index back."""
+    if ((_numpy.array(multiple) >= _numpy.array(lengths)).max() or
+         (_numpy.array(multiple) < 0).max()):
+         raise ValueError("indices are out of range")
+    lengths = _numpy.int64(_numpy.array(lengths))
+    index = 0
+    for i, length in enumerate(lengths):
+        index += _numpy.prod(lengths[i+1:]) * multiple[i]
+    return index
