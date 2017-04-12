3a4eb53f97628f11d31a6985e40fe0bf8b362301
Tomas Ekeberg <ekeberg@icm-241-157.icm.uu.se>
2017-04-12 15:27:30 +0200

    Added quaternion.py, a module to handle quaternion multiplication, inverse etc. This is still redundant with the rotations.py module but includes a quaternion class. With time I think this will replace rotations.py.

---
+++ eke/quaternion.py
@@ -0,0 +1,77 @@
+"""Tools to manipulate quaternions representing rotations. This is
+not a tool for general quaternions."""
+import numpy as _numpy
+
+class Quaternion(object):
+    """Rotation quaternion. Using automatic normalization"""
+    def __init__(self, elements):
+        if len(elements) != 4:
+            raise ValueError("Quaternion must be initialzied by a lenght 4 array.")
+        self._elements = _numpy.float64(elements)
+
+        self._elements[:] /= _numpy.sqrt((self._elements**2).sum())
+
+        if ((self._elements[0] < 0) or
+                (self._elements[0] == 0 and self._elements[1] < 0) or
+                (self._elements[0] == 0 and self._elements[1] == 0 and
+                 self._elements[2] < 0) or
+                (self._elements[0] == 0 and self._elements[1] == 0 and
+                 self._elements[2] == 0 and self._elements[3] < 0)):
+            self._elements[:] = -self._elements
+
+    def __str__(self):
+        return "Quaternion({0}, {1}, {2}, {3})".format(self._elements[0], self._elements[1], self._elements[2], self._elements[3])
+    __repr__ = __str__
+
+    def __getitem__(self, index):
+        if not isinstance(index, slice) and (index < 0 or index >= 4):
+            raise IndexError("Index must be 0, 1, 2 or 3. Not {0}".format(index))
+        return self._elements[index]
+
+    def __mul__(self, other):
+        return Quaternion((self[0]*other[0] - self[1]*other[1] - self[2]*other[2] - self[3]*other[3],
+                           self[0]*other[1] + self[1]*other[0] + self[2]*other[3] - self[3]*other[2],
+                           self[0]*other[2] - self[1]*other[3] + self[2]*other[0] + self[3]*other[1],
+                           self[0]*other[3] + self[1]*other[2] - self[2]*other[1] + self[3]*other[0]))
+
+    def __rmul__(self, other):
+        return Quaternion((other[0]*self[0] - other[1]*self[1] - other[2]*self[2] - other[3]*self[3],
+                           other[0]*self[1] + other[1]*self[0] + other[2]*self[3] - other[3]*self[2],
+                           other[0]*self[2] - other[1]*self[3] + other[2]*self[0] + other[3]*self[1],
+                           other[0]*self[3] + other[1]*self[2] - other[2]*self[1] + other[3]*self[0]))
+
+    def __invert__(self):
+        return Quaternion((self[0], -self[1], -self[2], -self[3]))
+
+
+def angle(quaternion):
+    """Get the rotation angle for a quaternion"""
+    return 2.*_numpy.arccos(quaternion[0])
+
+def axis(quaternion):
+    """Get the axis of rotation for a quaternion"""
+    return quaternion[1:] / _numpy.linalg.norm(quaternion[1:])
+
+def from_angle_and_dir(angle, direction):
+    """Create a Quaternion from an angle and a direction"""
+    return Quaternion((_numpy.cos(angle/2.),
+                       direction[0]/_numpy.linalg.norm(direction)*_numpy.sin(angle/2.),
+                       direction[1]/_numpy.linalg.norm(direction)*_numpy.sin(angle/2.),
+                       direction[2]/_numpy.linalg.norm(direction)*_numpy.sin(angle/2.)))
+
+def rotation_matrix(quaternion):
+    """Get the rotation matrix corresponding to a quaternion"""
+    return _numpy.matrix([[quaternion[0]**2 + quaternion[1]**2 - quaternion[2]**2 - quaternion[3]**2,
+                           2.0*quaternion[1]*quaternion[2] - 2.0*quaternion[0]*quaternion[3],
+                           2.0*quaternion[1]*quaternion[3] + 2.0*quaternion[0]*quaternion[2],],
+                          [2.0*quaternion[1]*quaternion[2] + 2.0*quaternion[0]*quaternion[3],
+                           quaternion[0]**2 - quaternion[1]**2 + quaternion[2]**2 - quaternion[3]**2,
+                           2.0*quaternion[2]*quaternion[3] - 2.0*quaternion[0]*quaternion[1]],
+                          [2.0*quaternion[1]*quaternion[3] - 2.0*quaternion[0]*quaternion[2],
+                           2.0*quaternion[2]*quaternion[3] + 2.0*quaternion[0]*quaternion[1],
+                           quaternion[0]**2 - quaternion[1]**2 - quaternion[2]**2 + quaternion[3]**2]])
+
+def average(quaternion_list):
+    """Generate the quaternion that best represent the average
+    of a list of quaternions"""
+    return Quaternion(_numpy.array([this_quaternion[:] for this_quaternion in quaternion_list]).mean(axis=0))
