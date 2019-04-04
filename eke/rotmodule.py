1a64fd6d2f2851b4281eebdea2f6930aaf8011f3
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2019-04-04 09:10:48 +0200

    Added tests of rotmodule. Fixed small thins in rotmodule.py

--- eke/rotmodule.py
+++ eke/rotmodule.py
@@ -1,9 +1,8 @@
 """A set of tools to handle quaternions and other functions related to
 spatial rotation."""
 import numpy as _numpy
-import warnings as _warnings
 
-def random(number_of_quaternions=1):
+def random(number_of_quaternions=1, fix_sign=True):
     random_base = _numpy.random.random((number_of_quaternions, 3))
     quats = _numpy.empty((number_of_quaternions, 4))
     quats[:, 0] = (_numpy.sqrt(1.-random_base[:, 0]) *
@@ -14,16 +13,21 @@ def random(number_of_quaternions=1):
                    _numpy.sin(2.*_numpy.pi*random_base[:, 2]))
     quats[:, 3] = (_numpy.sqrt(random_base[:, 0]) *
                    _numpy.cos(2.*_numpy.pi*random_base[:, 2]))
+    if fix_sign:
+        globals()["fix_sign"](quats)
     return quats.squeeze()
 
-def from_dir_and_angle(angle, direction):
+def from_angle_and_dir(angle, direction):
     """The quaternion corresponding to a rotations of angle (rad) around the
     axis defined by direction. The rotation follows the right-hand rule."""
+    angle = _numpy.mod(angle, 2.*_numpy.pi)
     quaternion = _numpy.zeros(4)
     #normalized_dir = _numpy.array(dir)/norm(dir)
     quaternion[0] = _numpy.cos(angle/2.)
-    normalization = _numpy.linalg.norm(direction)*_numpy.sqrt(1.-quaternion[0]**2)
-    quaternion[1:] = _numpy.array(direction)/normalization
+    normalization = _numpy.linalg.norm(direction)
+    modulation = _numpy.sqrt(1.-quaternion[0]**2)
+    quaternion[1:] = _numpy.array(direction)/normalization*modulation
+    fix_sign(quaternion)
     return quaternion
 
 def quaternion_to_euler_angle(quat):
@@ -45,7 +49,7 @@ def quaternion_to_euler_angle(quat):
 
 def quaternion_to_matrix(quat):
     """Dummy docstring"""
-    return _numpy.matrix([[quat[0]**2+quat[1]**2-quat[2]**2-quat[3]**2,
+    return _numpy.array([[quat[0]**2+quat[1]**2-quat[2]**2-quat[3]**2,
                           2.0*quat[1]*quat[2]-2.0*quat[0]*quat[3],
                           2.0*quat[1]*quat[3]+2.0*quat[0]*quat[2],],
                          [2.0*quat[1]*quat[2]+2.0*quat[0]*quat[3],
@@ -57,7 +61,7 @@ def quaternion_to_matrix(quat):
 
 def quaternion_to_matrix_bw(quat):
     """Dummy docstring"""
-    return _numpy.matrix([[quat[0]**2-quat[1]**2-quat[2]**2+quat[3]**2,
+    return _numpy.array([[quat[0]**2-quat[1]**2-quat[2]**2+quat[3]**2,
                           2.0*quat[2]*quat[3]+2.0*quat[0]*quat[1],
                           2.0*quat[1]*quat[3]-2.0*quat[0]*quat[2]],
                          [2.0*quat[2]*quat[3]-2.0*quat[0]*quat[1],
@@ -69,65 +73,78 @@ def quaternion_to_matrix_bw(quat):
 
 def inverse(quat_in):
     """Return the inverse of the quaternion. Input is unchanged."""
-    quat_out = _numpy.zeros(4)
-    quat_out[0] = quat_in[0]
-    quat_out[1:] = -quat_in[1:]
-    return quat_out
+    quat_out = _numpy.zeros(quat_in.shape)
+    quat_out[..., 0] = quat_in[..., 0]
+    quat_out[..., 1:] = -quat_in[..., 1:]
+    return quat_out.squeeze()
 
 def normalize(quat):
     """Normalize the quaternion and return the same object. (input quaternion is changed)"""
-    norm = _numpy.linalg.norm(quat)
+    if len(quat.shape) == 1:
+        quat = quat.reshape((1, 4))
+    norm = _numpy.linalg.norm(quat, axis=1)
     fix_sign(quat)
-    return quat/norm
+    return (quat/norm[:, _numpy.newaxis]).squeeze()
 
 def fix_sign(quat):
+    shape = (1, 4) if len(quat.shape) == 1 else quat.shape
+    quat_array = quat.reshape(shape)
+    under_consideration = _numpy.ones(quat_array.shape[0], dtype="bool8")
     for index in range(4):
-        if quat[index] < 0:
-            quat[:] = -quat
-            break
-        elif quat[index] > 0:
-            break
+        do_flip = under_consideration & (quat_array[..., index] < 0)
+        quat_array[do_flip, :] = -quat_array[do_flip, :]
+        under_consideration &= quat_array[..., index] == 0.
             
 def multiply(quat_1, quat_2):
     """Return the product of quat_1 and quat_2"""
-    quat_out = _numpy.zeros(4)
-    quat_out[0] = (quat_1[0]*quat_2[0] - quat_1[1]*quat_2[1] -
-                   quat_1[2]*quat_2[2] - quat_1[3]*quat_2[3])
-    quat_out[1] = (quat_1[0]*quat_2[1] + quat_1[1]*quat_2[0] +
-                   quat_1[2]*quat_2[3] - quat_1[3]*quat_2[2])
-    quat_out[2] = (quat_1[0]*quat_2[2] - quat_1[1]*quat_2[3] +
-                   quat_1[2]*quat_2[0] + quat_1[3]*quat_2[1])
-    quat_out[3] = (quat_1[0]*quat_2[3] + quat_1[1]*quat_2[2] -
-                   quat_1[2]*quat_2[1] + quat_1[3]*quat_2[0])
-    return quat_out
+    quat_1 = _numpy.array(quat_1)
+    quat_2 = _numpy.array(quat_2)
+    if len(quat_1.shape) == 0:
+        quat_1 = quat_1.reshape((1, 4))
+        quat_2 = quat_2.reshape((1, 4))
+    quat_out = _numpy.zeros(quat_1.shape)
+    quat_out[..., 0] = (quat_1[..., 0]*quat_2[..., 0] -
+                        quat_1[..., 1]*quat_2[..., 1] -
+                        quat_1[..., 2]*quat_2[..., 2] -
+                        quat_1[..., 3]*quat_2[..., 3])
+    quat_out[..., 1] = (quat_1[..., 0]*quat_2[..., 1] +
+                        quat_1[..., 1]*quat_2[..., 0] +
+                        quat_1[..., 2]*quat_2[..., 3] -
+                        quat_1[..., 3]*quat_2[..., 2])
+    quat_out[..., 2] = (quat_1[..., 0]*quat_2[..., 2] -
+                        quat_1[..., 1]*quat_2[..., 3] +
+                        quat_1[..., 2]*quat_2[..., 0] +
+                        quat_1[..., 3]*quat_2[..., 1])
+    quat_out[..., 3] = (quat_1[..., 0]*quat_2[..., 3] +
+                        quat_1[..., 1]*quat_2[..., 2] -
+                        quat_1[..., 2]*quat_2[..., 1] +
+                        quat_1[..., 3]*quat_2[..., 0])
+    return quat_out.squeeze()
 
 def relative(quat_1, quat_2):
     return multiply(inverse(quat_1), quat_2)
 
-def rotate(quat, point):
-    """Rotate a point by the quaternion"""
-    rotation_matrix = quaternion_to_matrix(quat)
-    return _numpy.squeeze(_numpy.array(
-        rotation_matrix*_numpy.transpose(_numpy.matrix(point))))
+# def rotate(quat, point):
+#     """Rotate a point by the quaternion"""
+#     rotation_matrix = quaternion_to_matrix(quat)
+#     return _numpy.squeeze(_numpy.array(
+#         rotation_matrix*_numpy.transpose(_numpy.matrix(point))))
 
-def rotate_array(quat, z_coordinates, y_coordinates, x_coordinates):
-    """Rotate coordinate vectors x, y and z by the rotation defined by the quaternion."""
-    rotation_matrix = quaternion_to_matrix(quat)
-    out_matrix = rotation_matrix*_numpy.matrix([z_coordinates, y_coordinates, x_coordinates])
-    out_array = _numpy.array(out_matrix)
-    return out_array[0], out_array[1], out_array[2]
+# def rotate(quat, z_coordinates, y_coordinates, x_coordinates):
+#     """Rotate coordinate vectors x, y and z by the rotation defined by the quaternion."""
+#     rotation_matrix = quaternion_to_matrix(quat)
+#     out_matrix = rotation_matrix*_numpy.matrix([z_coordinates, y_coordinates, x_coordinates])
+#     out_array = _numpy.array(out_matrix)
+#     return out_array[0], out_array[1], out_array[2]
 
-def rotate_array(quat, coordinates):
+def rotate(quat, coordinates):
     rotation_matrix = quaternion_to_matrix(quat)
-    out_matrix = rotation_matrix*_numpy.matrix(coordinates).transpose()
-    out_array = _numpy.array(out_matrix).transpose()
-    return out_array
+    return rotation_matrix.dot(coordinates.T).T
 
 def rotate_array_bw(quat, x_coordinates, y_coordinates, z_coordinates):
     """Like rotate_array but with the coordintes index backwords. Do not use."""
     rotation_matrix = quaternion_to_matrix_bw(quat)
-    out_matrix = rotation_matrix*_numpy.matrix([x_coordinates, y_coordinates, z_coordinates])
-    out_array = _numpy.array(out_matrix)
+    out_array = rotation_matrix.dot(numpy.array([x_coordinates, y_coordinates, z_coordinates]))
     return out_array[0], out_array[1], out_array[2]
 
 def read_quaternion_list(filename):
@@ -156,7 +173,8 @@ def n_to_angle(sampling_n):
     return 4./sampling_n/tau**3
 
 def rots_to_n(rots):
-    """The sampling parameter n corresponding to a certain number of rotations."""
+    """The sampling parameter n corresponding to a certain
+    number of rotations."""
     sampling_n = 1
     while True:
         this_rots = n_to_rots(sampling_n)
@@ -230,18 +248,14 @@ def matrix_to_euler_angle(mat, order="zxz"):
         
     return euler
 
-def _old_api(new_func, old_name):
-    def old_func(*args):
-        _warnings.warn("{} is deprecated. Please use {} instead".format(
-            old_name, new_func.__name__),
-                       DeprecationWarning)
-        return new_func(*args)
-    return old_func
+from . import refactor
+rotate_array = refactor.new_to_old(rotate, "rotate_array")
 
-random_quaternion = _old_api(random, "random_quaternion")
-quaternion_from_dir_and_angle = _old_api(from_dir_and_angle, "quaternion_from_dir_and_angle")
-quaternion_inverse = _old_api(inverse, "quaternion_inverse")
-quaternion_normalize = _old_api(normalize, "quaternion_normalize")
-quaternion_fix_sign = _old_api(fix_sign, "quaternion_fix_sign")
-quaternion_multiply = _old_api(multiply, "quaternion_multiply")
-quaternion_relative = _old_api(relative, "quaternion_relative")
+random_quaternion = refactor.new_to_old(random, "random_quaternion")
+quaternion_from_dir_and_angle = refactor.new_to_old(
+    from_angle_and_dir, "quaternion_from_dir_and_angle")
+quaternion_inverse = refactor.new_to_old(inverse, "quaternion_inverse")
+quaternion_normalize = refactor.new_to_old(normalize, "quaternion_normalize")
+quaternion_fix_sign = refactor.new_to_old(fix_sign, "quaternion_fix_sign")
+quaternion_multiply = refactor.new_to_old(multiply, "quaternion_multiply")
+quaternion_relative = refactor.new_to_old(relative, "quaternion_relative")
---
+++ tests/test_rotmodule.py
@@ -0,0 +1,107 @@
+from eke import rotmodule
+from eke import compare_rotations
+import numpy
+import unittest
+
+class TestRotmodule(unittest.TestCase):
+    def test_random(self):
+        """Check that all quaternions have non-negative x-component and that
+        the average is close to the x-axis"""
+        nrots = 100
+        #rots = numpy.array([rotmodule.random() for _ in range(nrots)])
+        rots = rotmodule.random(number_of_quaternions=nrots)
+        self.assertGreaterEqual(rots[:, 0].min(), 0)
+        numpy.testing.assert_array_almost_equal(rots.mean(axis=0)[1:],
+                                                numpy.array((0., 0., 0.,)),
+                                                decimal=1)
+        
+    def test_from_angle_and_dir(self):
+        rot = rotmodule.from_angle_and_dir(0, (1, 0, 0))
+        numpy.testing.assert_array_almost_equal(
+            rot, numpy.array([1., 0., 0., 0.]))
+        rot = rotmodule.from_angle_and_dir(numpy.pi, (0, 1, 0))
+        numpy.testing.assert_array_almost_equal(
+            rot, numpy.array([0., 0., 1., 0.]))
+
+    def test_inverse(self):
+        # Single
+        rot_1 = rotmodule.from_angle_and_dir(numpy.pi/8., (1., 0., 0.))
+        rot_2 = rotmodule.from_angle_and_dir(-numpy.pi/8., (1., 0., 0.))
+        rot_1_inv = rotmodule.inverse(rot_1)
+        numpy.testing.assert_array_almost_equal(rot_1_inv, rot_2)
+        # Array
+        rots = rotmodule.random(number_of_quaternions=10)
+        inv_1 = rotmodule.inverse(rots)
+        inv_2 = rots.copy();
+        inv_2[:, 0] = -inv_2[:, 0]
+        rotmodule.fix_sign(inv_2)
+        numpy.testing.assert_array_almost_equal(inv_1, inv_2)
+
+    def test_fix_sign(self):
+        # Single
+        rot = numpy.array([-1., 0., 0., 0.])
+        rotmodule.fix_sign(rot)
+        self.assertGreaterEqual(rot[0], 0)
+        rot = numpy.array([0., 0., 0., -1.])
+        rotmodule.fix_sign(rot)
+        numpy.testing.assert_array_almost_equal(
+            rot, numpy.array([0., 0., 0., 1.]))
+        # Array
+        rots = rotmodule.random(100, fix_sign=False)
+        rotmodule.fix_sign(rots)
+        self.assertGreaterEqual(rots[:, 0].min(), 0)
+
+    def test_normalize(self):
+        # Single
+        rot = numpy.random.random(4)
+        rot = rotmodule.normalize(rot)
+        self.assertAlmostEqual(numpy.sqrt((rot**2).sum()), 1.)
+        # Array
+        rots = numpy.random.random((10, 4))
+        rots = rotmodule.normalize(rots)
+        numpy.testing.assert_array_almost_equal(
+            numpy.sqrt((rots**2).sum(axis=1)), numpy.ones(10))
+
+    def test_multiply(self):
+        # Single
+        rot = rotmodule.random()
+        inv = rotmodule.inverse(rot)
+        identity = numpy.array((1., 0., 0., 0.))
+        numpy.testing.assert_array_almost_equal(
+            rotmodule.multiply(rot, inv), identity)
+        # Array
+        rot = rotmodule.random(number_of_quaternions=10)
+        inv = rotmodule.inverse(rot)
+        identity = numpy.zeros((10, 4))
+        identity[:, 0] = 1.
+        numpy.testing.assert_array_almost_equal(
+            rotmodule.multiply(rot, inv), identity)
+
+    def test_relative(self):
+        direction = numpy.random.random(3)
+        angle = numpy.random.uniform(0., numpy.pi)
+        rot_1 = rotmodule.from_angle_and_dir(angle, direction)
+        rot_2 = rotmodule.from_angle_and_dir(-angle, direction)
+        self.assertAlmostEqual(rotmodule.quaternion_to_angle(
+            rotmodule.relative(rot_1, rot_2)),
+                         2.*angle)
+        
+    def test_rotate_array(self):
+        # Single rot, Single point
+        rot = rotmodule.from_angle_and_dir(numpy.pi/2, (1., 0., 0.))
+        point = numpy.random.random(3)
+        target = numpy.array((point[0], -point[2], point[1]))
+        rotated = rotmodule.rotate(rot, point)
+        numpy.testing.assert_array_almost_equal(rotated, target)
+        # Single rot, Multiple points
+        rot = rotmodule.from_angle_and_dir(numpy.pi/2, (1., 0., 0.))
+        points = numpy.zeros((10, 3))
+        points[:, 1] = numpy.arange(10)
+        rotated = rotmodule.rotate(rot, points)
+        target = numpy.zeros((10, 3))
+        target[:, 2] = numpy.arange(10)
+        numpy.testing.assert_array_almost_equal(rotated, target)
+
+        
+if __name__ == "__main__":
+    unittest.main()
