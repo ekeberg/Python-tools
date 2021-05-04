da158e10d20837557771cc5da71241273f53b458
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2021-05-04 11:12:40 +0200

    Commented out half-written code from rotation_sampling.py

--- eke/rotation_sampling.py
+++ eke/rotation_sampling.py
@@ -1,3 +1,4 @@
+
 import numpy as _numpy
 import itertools as _itertools
 
@@ -160,7 +161,7 @@ def _face_indices():
     all_i2 = i2[mask]
     all_i3 = i3[mask]
     return all_i1, all_i2, all_i3
-    
+            
 
 def _cell_indices():
     verts = _base_vertices()
@@ -175,8 +176,9 @@ def _cell_indices():
              mask_1d[_numpy.newaxis, :, _numpy.newaxis, _numpy.newaxis]) *
             (mask_1d[_numpy.newaxis, :, _numpy.newaxis, _numpy.newaxis] >
              mask_1d[:, _numpy.newaxis, _numpy.newaxis, _numpy.newaxis]))
-
+    products *= mask
     mask = products > 3.67
+    # mask = products > 
 
     i1, i2, i3, i4 = _numpy.meshgrid(range(120), range(120), range(120), range(120), indexing="ij")
     all_i1 = i1[mask]
@@ -184,50 +186,55 @@ def _cell_indices():
     all_i3 = i3[mask]
     all_i4 = i4[mask]
     return all_i1, all_i2, all_i3, all_i4
-
-# def _celL_indices():
-#     verts = _base_vertices
-#     edges_ind_1, edge_ind_2 = _edge_indices()
     
 
+# def rotation_sampling(n):
+#     from eke import rotmodule
+#     import itertools
+#     import scipy.constants
 
-def rotation_sampling(n):
-    from eke import rotmodule
-    import itertools
-    import scipy.constants
+#     rotations = _numpy.zeros((2*rotmodule.n_to_rots(n), 4))
 
-    rotations = _numpy.zeros((2*rotmodule.n_to_rots(n), 4))
+#     # first 16
+#     rotations[:16, :] = _numpy.array(list(itertools.product(*((-0.5, 0.5), )*4)))
 
-    # first 16
-    rotations[:16, :] = _numpy.array(list(itertools.product(*((-0.5, 0.5), )*4)))
+#     # next 8
+#     rotations[16:20] = _numpy.identity(4)
+#     rotations[20:24] = -_numpy.identity(4)
 
-    # next 8
-    rotations[16:20] = _numpy.identity(4)
-    rotations[20:24] = -_numpy.identity(4)
-
-    # next 96
-    permutations = ((0, 1, 2, 3), (0, 3, 1, 2), (0, 2, 3, 1),
-                    (1, 2, 0, 3), (1, 3, 2, 0), (1, 0, 3, 2),
-                    (2, 0, 1, 3), (2, 3, 0, 1), (2, 1, 3, 0),
-                    (3, 1, 0, 2), (3, 2, 1, 0), (3, 0, 2, 1))
+#     # next 96
+#     permutations = ((0, 1, 2, 3), (0, 3, 1, 2), (0, 2, 3, 1),
+#                     (1, 2, 0, 3), (1, 3, 2, 0), (1, 0, 3, 2),
+#                     (2, 0, 1, 3), (2, 3, 0, 1), (2, 1, 3, 0),
+#                     (3, 1, 0, 2), (3, 2, 1, 0), (3, 0, 2, 1))
     
-    base = ((-0.5, 0.5),
-            (-0.5*scipy.constants.golden_ratio, 0.5*scipy.constants.golden_ratio),
-            (-0.5/scipy.constants.golden_ratio, 0.5/scipy.constants.golden_ratio),
-            0)
+#     base = ((-0.5, 0.5),
+#             (-0.5*scipy.constants.golden_ratio, 0.5*scipy.constants.golden_ratio),
+#             (-0.5/scipy.constants.golden_ratio, 0.5/scipy.constants.golden_ratio),
+#             0)
 
-    counter = 0
-    for v1, v2, v3 in itertools.product(*base[:3]):
-        b = (v1, v2, v3, 0)
-        for p in permutations:
-            print(counter)
-            rotations[24+counter] = (b[p[0]], b[p[1]], b[p[2]], b[p[3]])
-            counter += 1
+#     counter = 0
+#     for v1, v2, v3 in itertools.product(*base[:3]):
+#         b = (v1, v2, v3, 0)
+#         for p in permutations:
+#             print(counter)
+#             rotations[24+counter] = (b[p[0]], b[p[1]], b[p[2]], b[p[3]])
+#             counter += 1
 
-    # edges
-    edge_start_ind, endge_end_ind = _edge_indices()
-    for start_ind, end_ind in zip(edge_start_ind, edge_end_ind):
-        steps = linspace(0, 1, n+1)[1:-1]
-        steps[:, _numpy.newaxis]*
+#     # edges
+#     # edge_start_ind, endge_end_ind = _edge_indices()
+#     # for start_ind, end_ind in zip(edge_start_ind, edge_end_ind):
+#     #     steps = linspace(0, 1, n+1)[1:-1]
+#     #     steps[:, _numpy.newaxis]*
 
-    return rotations
+#     return rotations
+
+# def rotation_sampling(n):
+#     verts = _base_vertices()
+
+#     edges = _edge_indices()
+#     faces = _face_indices()
+#     cells = _cell_indices()
+
+#     split_cells = 
+    
