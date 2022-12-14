3cd4f5d9f182265f537fd11487bdc30d4c6b451a
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2022-12-14 09:55:59 +0100

    eke.rotate_map: Added function to rotate a map given correct and recovered orientations.

--- eke/rotate_map.py
+++ eke/rotate_map.py
@@ -1,9 +1,14 @@
 import numpy as _numpy
 from scipy.interpolate import RegularGridInterpolator
 from . import rotmodule
+from . import compare_rotations
 
 
 class MapRotater:
+    """Rotate a 3D map by a given quaternion. This is tested to be
+    consistent with how rotations are used in EMC.
+
+    """
     def __init__(self, data, fill_value=0):
         self._data = data
 
@@ -17,6 +22,7 @@ class MapRotater:
         self._points = _numpy.vstack((x.flatten(), y.flatten(), z.flatten()))
 
     def rotate(self, rot):
+        "Rotate the map by the given rotation."
         rotated_points = rotmodule.rotate(rot, self._points).T
         rotated_data = self._interpolater(rotated_points).reshape(
             self._data.shape)
@@ -24,5 +30,29 @@ class MapRotater:
 
 
 def rotate_map(data, rot):
+    """Rotate the data by the given quaternion."""
     return MapRotater(data).rotate(rot)
+
+
+def align_map(data, correct_rotations, recovered_rotations,
+              symmetry_operations=((1, 0, 0, 0), ), shallow_ewald=True,
+              return_rot_error=False):
+    """Rotate the map back to the orientation used for simulation by
+    providing the correct and recovered orientations. This function exists
+    to a large degree to help
+
+    """
+    relative_rot = (
+        compare_rotations.average_relative_orientation(correct_rotations,
+                                                       recovered_rotations,
+                                                       symmetry_operations,
+                                                       shallow_ewald))
+    rotated_data = rotate_map(data, relative_rot)
+    if return_rot_error:
+        aligned_rots = rotmodule.multiply(relative_rot, recovered_rotations)
+        errors = rotmodule.relative_angle(correct_rotations, aligned_rots)
+        average_error = _numpy.minimum(errors, _numpy.pi-errors).mean()
+        return rotated_data, average_error
+    else:
+        return rotated_data
 
